f4f6d46748c453a5dac341f62fa055f01f226aa7
Valeriy Bezuglyy <valnocorner@gmail.com>
2017-11-11 18:38:11 +0300

    prepare for config loading

--- Sources/FataMorganaFramework/FataMorgana.swift
+++ Sources/FataMorganaFramework/FataMorgana.swift
@@ -12,11 +12,41 @@ public final class FataMorgana {
     
     private let args: [String]
     
+    private var currentDirPath: String
+    private var fileManager: FileManager {
+        return FileManager.default
+    }
+    
+    let configName = "fatamorgana.yml"
+    
     public init(args: [String] = CommandLine.arguments) {
+        currentDirPath = "/"
         self.args = args
     }
     
     public func run() throws {
-
+        print("*** Fata Morgana **")
+        currentDirPath = fileManager.currentDirectoryPath
+        print("current dir = \(currentDirPath)")
+        
+        try loadConfig()
     }
+    
+    func loadConfig() throws{
+        print("\n=== Configuration")
+        print("= searching for \"\(configName)\"")
+        let configPath = currentDirPath + "/" + configName
+        if fileManager.fileExists(atPath: configPath) {
+            print("= found config")
+            print("= loading...")
+            print("= config loaded")
+        } else {
+            print("= config not found")
+            throw FataMorganaError.configNotFound
+        }
+    }
+}
+
+enum FataMorganaError: Error {
+    case configNotFound
 }
