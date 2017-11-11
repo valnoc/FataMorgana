1f2879e35ed9a97489e96df0186323080d887d98
Valeriy Bezuglyy <valnocorner@gmail.com>
2017-11-11 23:02:05 +0300

    added cuckoo generator

--- Package.swift
+++ Package.swift
@@ -6,7 +6,14 @@ let package = Package(
     name: "FataMorgana",
     
     dependencies: [
-        .package(url: "https://github.com/jpsim/Yams.git", from: "0.5.0")
+        .package(url: "https://github.com/jpsim/Yams.git", from: "0.5.0"),
+        
+        //>>> cuckoo
+        .package(url: "https://github.com/jpsim/SourceKitten.git", .upToNextMinor(from: "0.18.1")),
+        .package(url: "https://github.com/TadeasKriz/FileKit.git", .branch("develop")),
+        .package(url: "https://github.com/kylef/Stencil.git", from: "0.9.0"),
+        .package(url: "https://github.com/Carthage/Commandant.git", from: "0.12.0")
+        //<<<
     ],
     
     targets: [
@@ -18,8 +25,17 @@ let package = Package(
         
         .target(
             name: "FataMorganaFramework",
-            dependencies: ["Yams"],
+            dependencies: ["Yams", .target(name: "cuckoo_generator")],
             exclude: ["Tests"]
-        )
+        ),
+        
+        //>>> cuckoo
+        .target(name: "CuckooGeneratorFramework", dependencies: [
+            "FileKit", "SourceKittenFramework", "Stencil", "Commandant"], exclude: ["Tests"]),
+        
+        .target(name: "cuckoo_generator", dependencies: [
+            .target(name: "CuckooGeneratorFramework")], exclude: ["Tests"]),
+        //<<<
     ]
 )
+
