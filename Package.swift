5fa750f8af8eb50912a8d60565c2e347bd2b9d3d
Valeriy Bezuglyy <valnocorner@gmail.com>
2017-11-11 17:58:18 +0300

    update package

--- Package.swift
+++ Package.swift
@@ -1,15 +1,20 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "FataMorgana",
-  	targets: [
-		Target(name: "FataMorgana", dependencies: ["FataMorganaFramework"]),
-        Target(name: "FataMorganaFramework")
-    ],
-  	dependencies: [
-        .Package(url: "https://github.com/JohnSundell/Files.git", majorVersion: 1),
-        .Package(url: "https://github.com/jpsim/SourceKitten.git", majorVersion: 0)
+    targets: [
+        .target(
+            name: "FataMorgana",
+            dependencies: [.target(name: "FataMorganaFramework")],
+            exclude: ["Tests"]
+        ),
+        
+        .target(
+            name: "FataMorganaFramework",
+            dependencies: [],
+            exclude: ["Tests"]
+        )
     ]
 )
