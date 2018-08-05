8686010e5dbf5c1780a6ab512927e00519472693
Valeriy Bezuglyy <valnocorner@gmail.com>
2018-08-05 16:49:52 +0300

    move script inside test target

---
+++ example/FataExampleTests/mocks/MockSecondService.generated.swift
@@ -0,0 +1,28 @@
+// Generated using Sourcery 0.13.1 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+import Foundation
+import XCTest
+import Mirage
+
+//@testable import 
+// import additional modules
+
+class MockSecondService: SecondService, Mock {
+    lazy var mockManager: MockManager = MockManager(self, callRealFuncClosure: { [weak self] (funcName, args) -> Any? in
+        guard let __self = self else { return nil }
+        return nil
+    })
+    //MARK: - MockSecondService
+
+    let sel_makeRandomPositiveInt = "sel_makeRandomPositiveInt"
+    func makeRandomPositiveInt() -> Int {
+        return mockManager.handle(sel_makeRandomPositiveInt, withDefaultReturnValue: anyInt(), withArgs: nil) as! Int
+    }
+
+    let sel_foo = "sel_foo"
+    func foo() {
+        mockManager.handle(sel_foo, withDefaultReturnValue: nil, withArgs: nil)
+    }
+
+}
