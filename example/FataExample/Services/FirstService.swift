234b401fae6a66d71c8d5389e8da91b8e9438f69
Valeriy Bezuglyy <valnocorner@gmail.com>
2018-08-26 10:33:29 +0300

    fixed several bugs when generating super calls

--- example/FataExample/Services/FirstService.swift
+++ example/FataExample/Services/FirstService.swift
@@ -21,6 +21,39 @@ class FirstService {
     //sourcery: mirageSkip
     func skipMe() {
     }
+    
+    
+    func foo1() {
+        
+    }
+    
+    func foo2(array: [NSNumber], _ closure: @escaping Closure1) -> String {
+        return ""
+    }
+    
+    func foo3(number: NSNumber, closure: @escaping Closure1) {
+        
+    }
+    
+    //sourcery: mirageSel=foo3str
+    func foo3(string: String, closure: @escaping Closure1) {
+        
+    }
+    
+    func foo4(closure: @escaping Closure1) {
+        
+    }
+    
+    func foo5(withArray array: [NSNumber]) -> [String] {
+        return ["123"]
+    }
+    func foo6(withDict dict: [String: NSNumber]) -> [String: String] {
+        return ["123": "123"]
+    }
+    
+    func foo7(string: String) -> String {
+        return "12"
+    }
 }
 
 extension FirstService {
