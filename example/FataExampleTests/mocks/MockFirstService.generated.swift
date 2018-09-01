43e39b74b21234c2f90b26c2768288a5b801ee3d
Valeriy Bezuglyy <valnocorner@gmail.com>
2018-09-01 19:16:45 +0300

    fix

--- example/FataExampleTests/mocks/MockFirstService.generated.swift
+++ example/FataExampleTests/mocks/MockFirstService.generated.swift
@@ -2,7 +2,6 @@
 // DO NOT EDIT
 
 import Foundation
-import XCTest
 import Mirage
 
 @testable import FataExample
@@ -43,45 +42,54 @@ class MockFirstService: FirstService, Mock {
         }
     }
     //MARK: - MockFirstService
-
     let sel_performCalculation = "sel_performCalculation"
     override func performCalculation(arg1:Int, arg2: Int) -> Int {
         return mockManager.handle(sel_performCalculation, withDefaultReturnValue: anyInt(), withArgs: arg1, arg2) as! Int
     }
+
     let sel_performCalculation2 = "sel_performCalculation2"
     override func performCalculation2(arg1:Int, arg2: Int, arg333 arg3: Double) -> Int {
         return mockManager.handle(sel_performCalculation2, withDefaultReturnValue: anyInt(), withArgs: arg1, arg2, arg3) as! Int
     }
+
     let sel_foo1 = "sel_foo1"
     override func foo1() {
         mockManager.handle(sel_foo1, withDefaultReturnValue: nil, withArgs: nil)
     }
+
     let sel_foo2 = "sel_foo2"
     override func foo2(array: [NSNumber], _ closure: @escaping Closure1) -> String {
         return mockManager.handle(sel_foo2, withDefaultReturnValue: anyString(), withArgs: array, closure) as! String
     }
+
     let sel_foo3 = "sel_foo3"
     override func foo3(number: NSNumber, closure: @escaping Closure1) {
         mockManager.handle(sel_foo3, withDefaultReturnValue: nil, withArgs: number, closure)
     }
+
     let sel_foo3str = "sel_foo3str"
     override func foo3(string: String, closure: @escaping Closure1) {
         mockManager.handle(sel_foo3str, withDefaultReturnValue: nil, withArgs: string, closure)
     }
+
     let sel_foo4 = "sel_foo4"
     override func foo4(closure: @escaping Closure1) {
         mockManager.handle(sel_foo4, withDefaultReturnValue: nil, withArgs: closure)
     }
+
     let sel_foo5 = "sel_foo5"
     override func foo5(withArray array: [NSNumber]) -> [String] {
         return mockManager.handle(sel_foo5, withDefaultReturnValue: anyStringArray(), withArgs: array) as! [String]
     }
+
     let sel_foo6 = "sel_foo6"
     override func foo6(withDict dict: [String: NSNumber]) -> [String: String] {
         return mockManager.handle(sel_foo6, withDefaultReturnValue: [:], withArgs: dict) as! [String: String]
     }
+
     let sel_foo7 = "sel_foo7"
     override func foo7(string: String) -> String {
         return mockManager.handle(sel_foo7, withDefaultReturnValue: anyString(), withArgs: string) as! String
     }
+
 }
