d7060b941aa7a5e59063eea5a1ef330b858ad98a
Valeriy Bezuglyy <valnocorner@gmail.com>
2019-07-20 22:26:46 +0300

    cleanup

--- example/Pods/Target Support Files/Mirage/Mirage-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Mirage : NSObject
-@end
-@implementation PodsDummy_Mirage
-@end
--- example/Pods/Target Support Files/Pods-FataExampleTests/Pods-FataExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FataExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_FataExampleTests
-@end
