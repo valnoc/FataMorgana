1d299b797af81cb374548da368744f2eabd5a4d7
Valeriy Bezuglyy <valnocorner@gmail.com>
2019-07-20 22:28:54 +0300

    moved old examples

---
+++ example/Example/Services/ServiceInheritedImpl.swift
@@ -0,0 +1,14 @@
+//
+//  ServiceInheritedImpl.swift
+//  Example
+//
+//  Created by Valeriy Bezuglyy on 20/07/2019.
+//  Copyright © 2019 Valeriy Bezuglyy. All rights reserved.
+//
+
+import Foundation
+
+//sourcery: miragePartial
+class ServiceInheritedImpl: ServiceImpl {
+
+}
