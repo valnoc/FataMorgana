87a9008843c41562097dbc67fe81284a50462798
Valeriy Bezuglyy <valnocorner@gmail.com>
2019-09-01 09:47:01 +0300

    changed the way to write imports

--- example/ExampleTests/mocks/generated/MockFuncTuplesReturns.generated.swift
+++ example/ExampleTests/mocks/generated/MockFuncTuplesReturns.generated.swift
@@ -1,8 +1,7 @@
 // Generated using Sourcery 0.16.1 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 
-import Foundation
-import Mirage
+import Mirage 
 @testable import Example
 import Foundation
 
